8890e6ecf121abeaaefc9aeed4db53ca09afdf15
Sidhant <sidhant17.sk@gmail.com>
2022-12-19 17:42:33 +0530

    14 number program

---
+++ 14.js
@@ -0,0 +1,9 @@
+let angle1 = +prompt('Enter 1st angle of triangle');
+let angle2 = +prompt('Enter 2nd angle of triangle');
+let angle3 = +prompt('Enter 3rd angle of triangle');
+
+if (angle1 + angle2 + angle3 == 180 ) {
+    alert('Triangle is valid')
+} else {
+    alert('Triangle is not valid')
+}
