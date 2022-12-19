3eb2bcf4a1ecf900b1dc65924eaace6f04448614
Sidhant <sidhant17.sk@gmail.com>
2022-12-19 16:52:16 +0530

    3rd program

---
+++ 3.js
@@ -0,0 +1,9 @@
+let num = +prompt('Enter number');
+
+if (num == 0) {
+    alert('number is zero')
+} else if ( num > 0 ) {
+    alert('number is positive')
+} else {
+    alert('number is negative')
+}
