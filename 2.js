cb163fbb06a6a6e1328f64abcadfae521a75e377
Sidhant <sidhant17.sk@gmail.com>
2022-12-19 16:48:02 +0530

    2nd program

---
+++ 2.js
@@ -0,0 +1,12 @@
+let a = +prompt('enter  number: a');
+let b = +prompt('enter  number: b');
+let c = +prompt('Enter number: c');
+
+if ( a > b && a > c) {
+    alert(' a is largest number');
+} else if( b > a && b > c) {
+    alert('b is largest number');
+} else {
+    alert('c is largest number');
+}
+    
