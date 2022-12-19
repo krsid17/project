4d2a71a9c2b7cef8af8838b023d787a43df7c717
Sidhant <sidhant17.sk@gmail.com>
2022-12-19 17:40:11 +0530

    12 number prog

---
+++ 12.js
@@ -0,0 +1,29 @@
+let month_num =  +prompt('Enter year number from 1-12')
+
+if (month_num == 1 ) {
+    alert('Number of days in month is 31');
+} else if (month_num == 2 ) {
+    alert('Number of days in month is 28');
+} else if (month_num == 3 ) {
+    alert('Number of days in month is 31');
+} else if (month_num == 4 ) {
+    alert('Number of days in month is 30');
+} else if (month_num == 5 ) {
+    alert('Number of days in month is 31');
+} else if (month_num == 6 ) {
+    alert('Number of days in month is 30');
+} else if (month_num == 7 ) {
+    alert('Number of days in month is 31');
+} else if (month_num == 8 ) {
+    alert('Number of days in month is 31');
+} else if (month_num == 9 ) {
+    alert('Number of days in month is 30');
+} else if (month_num == 10 ) {
+    alert('Number of days in month is 31');
+} else if (month_num == 11 ) {
+    alert('Number of days in month is 30');
+} else if (month_num == 12 ) {
+    alert('Number of days in month is 31');
+} else{
+    alert('Enter a valid month number');
+}
