644c49d39e396190d166c26fe8eb22d347513899
Sidhant <sidhant17.sk@gmail.com>
2022-12-19 17:38:48 +0530

    11 number prog

---
+++ 11.js
@@ -0,0 +1,17 @@
+let week_num = +prompt('Enter week number from 1-7');
+
+if (week_num == 1) {
+    alert('Its Sunday');
+} else if (week_num == 2) {
+    alert('Its Monday')
+} else if (week_num == 3) {
+    alert('Its Tuesday')
+} else if (week_num == 4) {
+    alert('Its Wednesday')
+} else if (week_num == 5) {
+    alert('Its Thursday')
+} else if (week_num == 6) {
+    alert('Its Friday')
+}  else if (week_num == 7) {
+    alert('Its Saturday')
+}     
