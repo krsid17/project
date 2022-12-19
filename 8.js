cdb59e91909cf09fc4f090446dea75a8a1f39369
Sidhant <sidhant17.sk@gmail.com>
2022-12-19 17:29:31 +0530

    8th program

---
+++ 8.js
@@ -0,0 +1,9 @@
+let char = prompt('Enter alphabet ')
+
+if(char =='a' || char =='e' || char =='i' || char =='o' || char =='u') {
+    alert('Character is vowel')
+} else if (char =='A' || char =='E' || char =='I' || char =='O' || char =='U') {
+    alert('Character is vowel')
+} else {
+    alert('Charater is consonant')
+}
