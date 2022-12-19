768915ac7d558df303f0f247407a9b17ed52228e
Sidhant <sidhant17.sk@gmail.com>
2022-12-19 17:00:09 +0530

    new program

---
+++ 4.js
@@ -0,0 +1,7 @@
+let num = +prompt('Enter a positive number');
+
+if(num % 5 == 0  && num % 11 == 0 ) {
+    alert('Number is divisible by 5 and 11');
+} else {
+    alert('number is not divisible by 5 and 11');
+}
