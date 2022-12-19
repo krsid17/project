b77a28835327bac729b93d2ace6cdd64e5997197
krsid17 <99380459+krsid17@users.noreply.github.com>
2022-12-19 17:16:34 +0530

    Delete

--- 3.js
+++
@@ -1,9 +0,0 @@
-let num = +prompt('Enter number');
-
-if (num == 0) {
-    alert('number is zero')
-} else if ( num > 0 ) {
-    alert('number is positive')
-} else {
-    alert('number is negative')
-}
